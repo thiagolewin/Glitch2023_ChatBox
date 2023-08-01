7562109a44e9fe8a945b701581901d6df401e3b5
thiagolewin <thiagolewin06@gmail.com>
2023-07-31 22:25:38 -0300

    Subida

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import handlebars from 'express-handlebars'
 import __dirname from './utils.js'
 import {Server} from 'socket.io'
 const app = express()
-const PORT = 9090
+const PORT = process.env.PORT || 8080
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.engine("handlebars",handlebars.engine())
